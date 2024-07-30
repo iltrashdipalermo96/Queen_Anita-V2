09249e155ebdf81397349acb153e6ced9daad8aa
iltrashdipalermo96 <iltrashdipalermo810@gmail.com>
2024-07-30 11:31:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -54,7 +54,7 @@ module.exports = {
   botname: process.env.BOT_NAME || "TRASHBOT-V2",
   ownername: process.env.OWNER_NAME || "iltrashdipalermo",
   errorChat: process.env.ERROR_CHAT || "",
-  KOYEB_API: process.env.KOYEB_API || "6vn20v7z8qemgmxc61d1tmvqc9h4zxbwllan7dqyg35wjgrdmoki2m2g6v3uw7l2",
+  KOYEB_API: process.env.KOYEB_API || "true",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
